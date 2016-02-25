25353c539d2e8e03d6c91fd1ab92a1f471104ecf
Oleg Pudeyev <oleg@bsdpower.com>
2016-02-24 22:22:23 -0500

    Host for the extension

---
+++ host.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import bottle
+
+app = bottle.Bottle()
+app.debug = True
+
+@app.route('/')
+def index():
+    if 'now' in bottle.request.cookies:
+        status = 'Cookie is set: %s' % bottle.request.cookies['now']
+    else:
+        status = 'Cookie is not set'
+        
+    return '<html><body><p>%s</p><p><a href="/set-cookie">Set cookie</a></p></body></html>' % status
+
+@app.route('/status')
+def status():
+    if 'now' in bottle.request.cookies:
+        status = 'Cookie is set: %s' % bottle.request.cookies['now']
+    else:
+        status = 'Cookie is not set'
+        
+    return status
+
+app.run(port=8112)
