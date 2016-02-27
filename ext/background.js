f1ef2ee49439f87aa5affbdb986f986d0f12f1fa
Oleg Pudeyev <oleg@bsdpower.com>
2016-02-26 22:44:04 -0500

    Generalize host url checking

--- ext/background.js
+++ ext/background.js
@@ -1,20 +1,34 @@
-var allowedUrls = [
+var allowedRemoteUrls = [
   'http://localhost:8192/',
+];
+var allowedHostUrls = [
+  'http://striker:8112',
 ];
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     // if sender.tab is set, the message came from a content script;
     // otherwise, the message came from an extension(?)
-    if (!sender.tab || sender.tab.url.indexOf('http://striker:8112') != 0) {
+    if (!sender.tab) {
+      return;
+    }
+    
+    var hostOk = false;
+    for (var i = 0; i < allowedHostUrls.length; ++i) {
+      if (sender.tab.url.indexOf(allowedHostUrls[0]) === 0) {
+        hostOk = true;
+        break;
+      }
+    }
+    if (!hostOk) {
       return;
     }
     
     if (request.action == "getCookies")
       var url = request.url;
       var urlOk = false;
-      for (var i = 0; i < allowedUrls.length; ++i) {
-        if (url === allowedUrls[i]) {
+      for (var i = 0; i < allowedRemoteUrls.length; ++i) {
+        if (url === allowedRemoteUrls[i]) {
           urlOk = true;
           break;
         }
